95d0b92163885b7542dc69585c80551838e1b02a
andriyDev <andriydzikh@gmail.com>
2023-05-01 22:07:22 -0700

    Use recastnavigation-sys instead of recast-detour-sys.

--- crates/recast-detour-rs/src/lib.rs
+++ crates/recast-detour-rs/src/lib.rs
@@ -1,21 +1,26 @@
+use recastnavigation_sys::*;
 use std::collections::HashMap;
-use std::ffi::CStr;
-use std::os::raw::c_void;
 use std::ptr;
 
-mod util;
 mod nav_obj;
+mod util;
 
 pub use nav_obj::NavObjFile;
 
+const POLYFLAGS_WALK: u16 = 0x1;
+
 #[derive(Debug)]
 pub struct RecastQuery {
-    q: ptr::NonNull<c_void>,
+    mesh: ptr::NonNull<dtNavMesh>,
+    q: ptr::NonNull<dtNavMeshQuery>,
 }
 
 impl Drop for RecastQuery {
     fn drop(&mut self) {
-        unsafe { sys::recastc_free_query(self.q.as_ptr()) }
+        unsafe {
+            dtFreeNavMeshQuery(self.q.as_ptr());
+            dtFreeNavMesh(self.mesh.as_ptr());
+        }
     }
 }
 
@@ -153,33 +158,142 @@ impl RecastQuery {
 
         let (cu_verts, indices) = remove_dup(&cu_verts, &data.indices);
 
-        let vert_count = (cu_verts.len() / 3) as u32;
-        let triangles_count = (data.indices.len() / 3) as u32;
+        let vert_count = cu_verts.len() / 3;
+        let triangles_count = data.indices.len() / 3;
 
-        let sys_data = sys::RecastNavMeshData {
+        let mut polys = Vec::with_capacity(triangles_count * 6);
+        for i in 0..triangles_count as usize {
+            polys.push(indices[i * 3 + 0]);
+            polys.push(indices[i * 3 + 1]);
+            polys.push(indices[i * 3 + 2]);
+            polys.push(0);
+            polys.push(0);
+            polys.push(0);
+        }
+
+        util::build_mesh_adjacency(&mut polys, vert_count, 3);
+
+        let poly_flags = vec![POLYFLAGS_WALK; triangles_count];
+
+        // TODO(edwin):
+        // We assume all polygon are in the same area
+        let poly_areas = vec![0; triangles_count];
+
+        let mut params = dtNavMeshCreateParams {
             verts: cu_verts.as_ptr(),
-            vert_count,
-            indices: indices.as_ptr(),
-            triangles_count,
+            vertCount: vert_count as i32,
+            polys: polys.as_ptr(),
+            polyFlags: poly_flags.as_ptr(),
+            polyAreas: poly_areas.as_ptr(),
+            polyCount: triangles_count as i32,
+            nvp: 3,
+            detailMeshes: std::ptr::null(),
+            detailVerts: std::ptr::null(),
+            detailVertsCount: 0,
+            detailTris: std::ptr::null(),
+            detailTriCount: 0,
+            offMeshConVerts: std::ptr::null(),
+            offMeshConRad: std::ptr::null(),
+            offMeshConFlags: std::ptr::null(),
+            offMeshConAreas: std::ptr::null(),
+            offMeshConDir: std::ptr::null(),
+            offMeshConUserID: std::ptr::null(),
+            offMeshConCount: 0,
+            userId: 0,
+            tileX: 0,
+            tileY: 0,
+            tileLayer: 0,
             bmin,
             bmax,
-            walkable_height: data.walkable_height,
-            walkable_radius: data.walkable_radius,
-            walkable_climb: data.walkable_climb,
-            cell_size: data.cell_size,
-            cell_height: data.cell_height,
+            walkableHeight: data.walkable_height,
+            walkableRadius: data.walkable_radius,
+            walkableClimb: data.walkable_climb,
+            cs: data.cell_size,
+            ch: data.cell_height,
+            buildBvTree: true,
         };
 
-        let mut err = sys::RecastNavError::zeros();
+        let mut nav_data = std::ptr::null_mut();
+        let mut nav_data_size = 0;
 
-        let q = unsafe {
-            ptr::NonNull::new(
-                sys::recastc_create_query(&sys_data as *const _, &mut err as *mut _) as *mut c_void,
+        // SAFETY: dtCreateNavMeshData manages its own memory and only mutates `nav_data` and
+        // `nav_data_size`. `params` is only read.
+        if !unsafe { dtCreateNavMeshData(&mut params, &mut nav_data, &mut nav_data_size) } {
+            return Err(Error::CreateQueryError(
+                "dtCreateNavMeshData failed!".to_owned(),
+            ));
+        }
+
+        if nav_data.is_null() {
+            return Err(Error::CreateQueryError(
+                "dtCreateNavMeshData returned null data".to_owned(),
+            ));
+        }
+        if nav_data_size == 0 {
+            return Err(Error::CreateQueryError(
+                "dtCreateNavMeshData returned zero size data".to_owned(),
+            ));
+        }
+
+        // SAFETY: dtAllocNavMesh either allocates its own memory or returns null, which is handled
+        // by NonNull.
+        let mut mesh = match ptr::NonNull::new(unsafe { dtAllocNavMesh() }) {
+            Some(mesh) => mesh,
+            None => {
+                return Err(Error::CreateQueryError(
+                    "Could not allocate dtNavMesh".to_owned(),
+                ))
+            }
+        };
+        // SAFETY: We can turn the mesh into a mutable borrow, since only this scope has the
+        // pointer, so it is exclusive. `init1` takes ownership of nav_data and otherwise only
+        // mutates `mesh`.
+        let status = unsafe {
+            mesh.as_mut().init1(
+                nav_data,
+                nav_data_size,
+                dtTileFlags_DT_TILE_FREE_DATA as i32,
             )
         };
+        if (status & DT_FAILURE) != 0 {
+            // SAFETY: These pointers are only present here, and the memory was allocated by
+            // Detour, so freeing is safe.
+            unsafe {
+                dtFree(nav_data as _);
+                dtFreeNavMesh(mesh.as_ptr());
+            }
+            return Err(Error::CreateQueryError(
+                "Could not init Detour NavMesh".to_owned(),
+            ));
+        }
 
-        let q = q.ok_or(Error::CreateQueryError(err.msg().into_owned()))?;
-        Ok(RecastQuery { q })
+        // SAFETY: dtAllocNavMeshQuery either allocates its own memory or returns null, which is
+        // handled by NonNull.
+        let mut q = match ptr::NonNull::new(unsafe { dtAllocNavMeshQuery() }) {
+            Some(q) => q,
+            None => {
+                return Err(Error::CreateQueryError(
+                    "Could not allocate dtNavMesh".to_owned(),
+                ))
+            }
+        };
+        // SAFETY: We can turn the mesh into a mutable borrow, since only this scope has the
+        // pointer, so it is exclusive. `init` only mutates `q` and otherwise just reads from
+        // `mesh`.
+        let status = unsafe { q.as_mut().init(mesh.as_ptr(), 1000) };
+        if (status & DT_FAILURE) != 0 {
+            // SAFETY: These pointers are only present here, and the memory was allocated by
+            // Detour, so freeing is safe.
+            unsafe {
+                dtFreeNavMeshQuery(q.as_ptr());
+                dtFreeNavMesh(mesh.as_ptr());
+            }
+            return Err(Error::CreateQueryError(
+                "Could not init Detour NavMesh".to_owned(),
+            ));
+        }
+
+        Ok(RecastQuery { mesh, q })
     }
 
     pub fn find_path(&self, start: Point, end: Point, r: f32) -> Result<Vec<Point>> {
@@ -190,41 +304,46 @@ impl RecastQuery {
             return Ok(vec![end_p]);
         }
 
-        let mut result = sys::RecastPathResult::default();
-        let mut err = sys::RecastNavError::zeros();
-
-        let input = sys::RecastPathInput {
-            start_poly,
-            start_pos: start_p.0,
-            end_poly,
-            end_pos: end_p.0,
+        let filter = dtQueryFilter {
+            m_areaCost: [1.0; 64],
+            m_includeFlags: POLYFLAGS_WALK,
+            m_excludeFlags: 0,
         };
 
-        let res = unsafe {
-            sys::recastc_find_path(
-                self.q.as_ptr(),
-                &input as *const _,
-                &mut result as *mut _,
-                &mut err as *mut _,
+        let mut path = vec![0; 100];
+        let mut count = 0;
+
+        let status = unsafe {
+            (*self.q.as_ptr()).findPath(
+                start_poly,
+                end_poly,
+                start_p.0.as_ptr(),
+                end_p.0.as_ptr(),
+                &filter,
+                path.as_mut_ptr(),
+                &mut count,
+                100,
             )
         };
+        path.truncate(count as usize);
 
-        if res == 0 {
-            let error = err.msg().to_string();
-            if error == "PARTIAL_RESULT" {
-                return Err(Error::PartialResult);
-            }
-
-            return Err(Error::FindPathError(error));
-        }
-
-        let path = &result.path[0..result.path_count as usize];
-
-        match path.len() {
-            0 => Err(Error::FindPathError("No Path".to_string())),
-            // Same Poly, so just return the next point
-            1 => Ok(vec![end_p]),
-            _ => {
+        if status & DT_FAILURE != 0 {
+            Err(Error::FindPathError("FAIL_TO_FIND_PATH".to_owned()))
+        } else if status & DT_INVALID_PARAM != 0 {
+            Err(Error::FindPathError("INVALID_PARAM".to_owned()))
+        } else if status & DT_BUFFER_TOO_SMALL != 0 {
+            Err(Error::FindPathError("BUFFER_TOO_SMALL".to_owned()))
+        } else if status & DT_OUT_OF_NODES != 0 {
+            Err(Error::FindPathError("OUT_OF_NODES".to_owned()))
+        } else if status & DT_PARTIAL_RESULT != 0 {
+            Err(Error::PartialResult)
+        } else if path.len() == 0 {
+            Err(Error::FindPathError("NoPath".to_owned()))
+        } else {
+            if path.len() == 1 {
+                // Same Poly, so just return the next point
+                Ok(vec![end_p])
+            } else {
                 // remap the poly and the points
                 let mut res = vec![];
                 let mut p = start_p;
@@ -239,60 +358,76 @@ impl RecastQuery {
     }
 
     fn find_closest(&self, pos: Point, target_poly: u32) -> Result<Point> {
-        let input = sys::RecastClosestPointInput {
-            pos: pos.0,
-            poly: target_poly,
-        };
-
-        let mut result = sys::RecastClosestPointResult::default();
-        let mut err = sys::RecastNavError::zeros();
-        let res = unsafe {
-            sys::recastc_find_closest_point(
-                self.q.as_ptr(),
-                &input as *const _,
-                &mut result as *mut _,
-                &mut err as *mut _,
+        let mut closest = [0.0; 3];
+        let status = unsafe {
+            (*self.q.as_ptr()).closestPointOnPoly(
+                target_poly,
+                pos.0.as_ptr(),
+                closest.as_mut_ptr(),
+                std::ptr::null_mut(),
             )
         };
 
-        if res == 0 {
-            Err(Error::FindPointError(err.msg().to_string()))
-        } else {
-            Ok(Point(result.pos))
+        if status & DT_INVALID_PARAM != 0 {
+            return Err(Error::FindPointError(
+                "Fail to find path: reason: [Invalid Param]".to_owned(),
+            ));
         }
+        if status & DT_FAILURE != 0 {
+            return Err(Error::FindPointError(
+                "Fail to find closest point: reason[unknown]".to_owned(),
+            ));
+        }
+
+        Ok(Point(closest))
     }
 
     pub fn find_poly(&self, pos: Point, r: f32) -> Result<(Point, u32)> {
-        let mut result = sys::RecastNearestPolyResult::default();
-        let mut err = sys::RecastNavError::zeros();
-
-        let input = sys::RecastNearestPolyInput {
-            center: pos.0,
-            half_extents: [r, r, r],
+        let filter = dtQueryFilter {
+            m_areaCost: [1.0; 64],
+            m_includeFlags: POLYFLAGS_WALK,
+            m_excludeFlags: 0,
         };
-
-        let res = unsafe {
-            sys::recastc_find_nearest_poly(
-                self.q.as_ptr(),
-                &input as *const _,
-                &mut result as *mut _,
-                &mut err as *mut _,
+        let mut result_poly = 0;
+        let mut result_pos = [0.0; 3];
+        // SAFETY: Dereferencing is safe for Rust since Self only stores pointers.
+        // `findNearestPoly` only mutates `result_poly` and `result_pos`.
+        let status = unsafe {
+            (*self.q.as_ptr()).findNearestPoly(
+                pos.0.as_ptr(),
+                [r, r, r].as_ptr(),
+                &filter,
+                &mut result_poly,
+                result_pos.as_mut_ptr(),
             )
         };
 
-        match res {
-            0 => Err(Error::FindPointError(err.msg().to_string())),
-            _ if result.poly == 0 => Err(Error::FindPointError("No poly found".into())),
-            _ => Ok((Point(result.pos), result.poly)),
+        if status & DT_FAILURE != 0 {
+            return Err(Error::FindPointError("FAIL_TO_FIND_PATH".to_owned()));
+        }
+        if status & DT_INVALID_PARAM != 0 {
+            return Err(Error::FindPointError("INVALID_PARAM".to_owned()));
+        }
+        if status & DT_BUFFER_TOO_SMALL != 0 {
+            return Err(Error::FindPointError("BUFFER_TOO_SMALL".to_owned()));
+        }
+        if status & DT_OUT_OF_NODES != 0 {
+            return Err(Error::FindPointError("OUT_OF_NODES".to_owned()));
+        }
+        if status & DT_PARTIAL_RESULT != 0 {
+            return Err(Error::FindPointError("PARTIAL_RESULT".to_owned()));
+        }
+
+        if result_poly == 0 {
+            Err(Error::FindPointError("No poly found".to_owned()))
+        } else {
+            Ok((Point(result_pos), result_poly))
         }
     }
 }
 
 pub fn version() -> String {
-    let version = unsafe { sys::recastc_version() };
-    assert_ne!(version, ptr::null());
-    let version = unsafe { CStr::from_ptr(version).to_str().unwrap() };
-    version.to_string()
+    "0.0.1".to_owned()
 }
 
 #[cfg(test)]
